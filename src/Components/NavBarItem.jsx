7f1b20fd13cf4ed83ee7ce21d3b575c8247fbf22
samergoda <judosamer555@gmail.com>
2024-04-07 11:18:55 +0200

    nav

--- src/Components/NavBarItem.jsx
+++ src/Components/NavBarItem.jsx
@@ -1,32 +1,21 @@
-"use client";
 import Link from "next/link";
-import { Suspense } from 'react'; 
 import { useSearchParams } from "next/navigation";
 
-function SuspenseWrapper({ children }) {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {children}
-    </Suspense>
-  );
-}
-
 export default function NavBarItem({ title, param }) {
-  return (
-    <SuspenseWrapper>
-      <NavBarItemContent title={title} param={param} />
-    </SuspenseWrapper>
-  );
+  return <NavBarItemContent title={title} param={param} />;
 }
 
 function NavBarItemContent({ title, param }) {
   const searchParams = useSearchParams();
   let genre = searchParams.get("genre");
+  
   return (
     <Link
-      className={`hover:text-amber-600 font-semibold  ${
-        genre === param ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg " : ""
-      }}`}
+      className={`hover:text-amber-600 font-semibold ${
+        genre === param
+          ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+          : ""
+      }`}
       href={`/?genre=${param}`}
     >
       {title}
